08fc4863a726f5fe4c4213544a59ddf0785f8cf3
João Pedro Xavier <joaopedroaxavier@gmail.com>
2018-01-02 03:45:40 -0200

    Added A, B, C from #411 Div.1

---
+++ CodeForces/411div1/a.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define topper top //XDDDDDDD
+
+#define mp make_pair
+#define pb push_back
+#define db(x) cerr << #x << " == " <<  x << endl;
+#define _ << " " <<
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> ii;
+
+const long double EPS = 1e-9;
+const int N=1e5+5;
+const int MOD=1e9+7;
+const int INF=0x3f3f3f3f;
+
+int n;
+
+int main(){
+  scanf("%d", &n);
+  printf("%d\n", n&1 ? n/2 : n/2 - 1);
+
+	return 0;
+}
+
